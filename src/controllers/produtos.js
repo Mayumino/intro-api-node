42548c8b54aa7c891a63c7f72b17df071720b647
Mayumino <uenomiyano@gmail.com>
2025-04-17 20:51:58 -0300

    17.04

--- src/controllers/produtos.js
+++ src/controllers/produtos.js
@@ -2,63 +2,69 @@ const db = require('../database/connection');
 
 module.exports = {
    async listarProdutos (request, response) {
-  try{
+      try {
+         const sql = `
+            SELECT
+            prod_id, emp_id, cat_id, prod_nome, prod_desc, prod_est, prod_preco
+            FROM PRODUTOS;
+            `;
+         const [row] = await db.query(sql);
+         const nItens = row.length
 
-   const sql = `
-      SELECT
-      prod_id, emp_id, cat_id, prod_nome, prod_desc, prod_est, prod_preco
-      FROM PRODUTOS;
-   `;
+         return response.status(200).json({
+         sucesso: true,
+         mensagem:'lista de produtos',
+         nItens,
+         dados: row
+         });
 
-   const [row] = await db.query(sql);
-   const nItens = row.length
-
-    return response.status(200).json({
-
-        sucesso: true,
-        mensagem:'lista de produtos',
-        nItens,
-        dados: row
-
-     })
+      } catch (error){
+          return response.status(500).json({
+            sucesso: false,
+            mensagem:'erro na listagem de produtos',
+            dados: error.message
+          });
       }
-
-     catch (error){
-         
-     return response.status(500).json({
-
-        sucesso: false,
-        mensagem:'erro na listagem de produtos',
-        dados: error.message
-     })
-
-     }
-
-    },
+   },
 
 
    async cadastrarProdutos (request, response) {
-  try{
-    return response.status(200).json({
+      try{
 
-        sucesso: true,
-        mensagem:'Cadastro de Produto',
-        dados: null
+         const {empresa, categoria, nomeProduto, descricao, estoque, preco} = request.body;
 
-     })
+         const sql = `
+            INSERT INTO produtos
+               (emp_id, cat_id, prod_nome, prod_desc, prod_est, prod_preco)
+            VALUES
+               (?, ?, ?, ? , ? ,?);
+         `
+         const values = [empresa, categoria, nomeProduto, descricao, estoque, preco];
+         const [result] = await db.query(sql, values);
+
+         const dados = {
+            id: result.insertId,
+            empresa,
+            categoria,
+            nomeProduto,
+            descricao,
+            estoque,
+            preco
+         };
+
+         return response.status(200).json({
+          sucesso: true,
+          mensagem:'Cadastro de Produto',
+          dados: dados
+      });
+      
+      } catch (error){
+         return response.status(500).json({
+          sucesso: false,
+          mensagem:'erro no cadastro do produto',
+          dados: error.message
+         });
       }
-
-     catch (error){
-         
-     return response.status(500).json({
-
-        sucesso: false,
-        mensagem:'erro no cadastro do produto',
-        dados: error.message
-     })
-
-     }
-
     },
 
 
